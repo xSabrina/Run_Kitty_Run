5472c89015881a1f326abf067e657f433d8a50aa
Pascal Lindemann <38719784+PascalLindemann@users.noreply.github.com>
2020-06-15 12:24:19 +0200

    Enemies animation fixed + Collider Bug fixed

--- Run_Kitty_Run/Assets/Scripts/EnemyRotation.cs
+++ Run_Kitty_Run/Assets/Scripts/EnemyRotation.cs
@@ -21,30 +21,30 @@ public class EnemyRotation : MonoBehaviour
         var direction = rb.velocity;
         var angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         if (animator.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
-
-        if(angle == 0)
         {
-            // idle
-            transform.rotation = Quaternion.Euler(Vector3.zero);
+            if (angle == 0)
+            {
+                // idle
+                transform.rotation = Quaternion.Euler(Vector3.zero);
+            }
+            else if (angle < -225 && angle > -315 || angle < 135 && angle > 45)
+            {
+                animator.SetBool("isWalkingUp", true);
+            }
+            else if (angle >= -45 && angle <= 45)
+            {
+                transform.rotation = Quaternion.Euler(Vector3.zero);
+                animator.SetBool("isWalkingSide", true);
+            }
+            else if (angle < -45 && angle > -125)
+            {
+                animator.SetBool("isWalkingDown", true);
+            }
+            else
+            {
+                transform.rotation = Quaternion.Euler(0, 180, 0);
+                animator.SetBool("isWalkingSide", true);
+            }
         }
-        else if (angle < -225 && angle > -315 || angle < 135 && angle > 45)
-        {
-            animator.SetBool("isWalkingUp", true);
-        }
-        else if (angle >= -45 && angle <= 45)
-        {
-            transform.rotation = Quaternion.Euler(Vector3.zero);
-            animator.SetBool("isWalkingSide", true);
-        }
-        else if (angle < -45 && angle > -125)
-        {
-            animator.SetBool("isWalkingDown", true);
-        }
-        else
-        {
-            transform.rotation = Quaternion.Euler(0, 180, 0);
-            animator.SetBool("isWalkingSide", true);
-        }
-        
     }
 }
