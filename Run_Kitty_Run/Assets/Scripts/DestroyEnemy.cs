b639cd4e61b11ec362fecdc25c42093515722f91
flexwend <felix.wende@web.de>
2020-06-16 16:55:25 +0200

    Update DestroyEnemy.cs
    ignore collision with player

--- Run_Kitty_Run/Assets/Scripts/DestroyEnemy.cs
+++ Run_Kitty_Run/Assets/Scripts/DestroyEnemy.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class DestroyEnemy : MonoBehaviour
 {
+    private void Start()
+    {
+        var player = GameObject.FindGameObjectWithTag("Player");
+        Physics2D.IgnoreCollision(player.GetComponent<Collider2D>(), GetComponent<Collider2D>());
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         Debug.Log(collision.transform.name);
