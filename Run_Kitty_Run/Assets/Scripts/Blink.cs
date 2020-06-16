5e471132dd6e1177556568c4d713c6a49cf11a86
flexwend <felix.wende@web.de>
2020-06-16 22:49:26 +0200

    tests with raycast

--- Run_Kitty_Run/Assets/Scripts/Blink.cs
+++ Run_Kitty_Run/Assets/Scripts/Blink.cs
@@ -1,11 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 [CreateAssetMenu(menuName = "Abilities/Blink")]
 public class Blink : Ability
 {
     public float aRange = 0f;
-
+    public LayerMask mylayerMask;
     private SelfCastTriggerable launcher;
 
     public override void Initialize(GameObject obj)
@@ -15,8 +16,21 @@ public class Blink : Ability
 
     public override void TriggerAbility()
     {
-        Rigidbody2D rbPlayer = launcher.player.GetComponent<Rigidbody2D>();
-        rbPlayer.position += (Vector2)launcher.spawnPoint.transform.up.normalized * aRange; 
+        Transform playerTransform = launcher.player.GetComponent<Transform>();
+        
+        ContactFilter2D filter = new ContactFilter2D();
+        List<RaycastHit2D> res = new List<RaycastHit2D>();
+        int hitint = Physics2D.Raycast(playerTransform.position, launcher.spawnPoint.transform.up, filter.NoFilter(), res, aRange);
+        foreach(RaycastHit2D hit in res)
+        {
+            if(hit.collider.name == "Tilemap")
+            {
+                Debug.Log(hit.collider.name);
+                playerTransform.position += launcher.spawnPoint.transform.up * hit.collider.Distance(launcher.player.GetComponent<CapsuleCollider2D>()).distance;
+            }
+        }
+
+        //playerTransform.position += launcher.spawnPoint.transform.up.normalized * aRange;
     }
 
 }
