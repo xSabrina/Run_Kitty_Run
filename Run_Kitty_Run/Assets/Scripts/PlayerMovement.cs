13ce08de26749dea15c6e5bf70befb718b6c72c6
Sabrina Freisleben <41908872+xSabrina@users.noreply.github.com>
2020-06-26 13:50:20 +0200

    Deleting gamepad elements

--- Run_Kitty_Run/Assets/Scripts/PlayerMovement.cs
+++ Run_Kitty_Run/Assets/Scripts/PlayerMovement.cs
@@ -14,11 +14,9 @@ public class PlayerMovement : MonoBehaviour
     Animator playerAnimator;
     PlayerInputActions inputAction;
     Vector2 movementInput;
-    Vector2 rotateInput;
     Vector3 movement;
     Vector3 inputDirection;
     
-
     void Start(){
         spriteRenderer = GetComponent<SpriteRenderer>();
         playerAnimator = GetComponent<Animator>();
@@ -27,7 +25,6 @@ public class PlayerMovement : MonoBehaviour
     void Awake() {
         inputAction = new PlayerInputActions();
         inputAction.PlayerControls.Move.performed += ctx => movementInput = ctx.ReadValue<Vector2>();
-        inputAction.PlayerControls.Rotate.performed += ctx => rotateInput = ctx.ReadValue<Vector2>();
     }
 
     void FixedUpdate() 
@@ -42,7 +39,6 @@ public class PlayerMovement : MonoBehaviour
 
     void MoveThePlayer(Vector3 desiredDirection) 
     {
-        Vector3 movement = new Vector3();
         movement.Set(desiredDirection.x, desiredDirection.y, 0f);
         movement = movement * speed * Time.deltaTime;
         playerRigidbody.MovePosition(transform.position + movement);
@@ -51,14 +47,14 @@ public class PlayerMovement : MonoBehaviour
     void AnimateThePlayer(Vector3 desiredDirection)
     {
         ClearAnimations();
-        if(Keyboard.current.wKey.isPressed || Gamepad.current.leftStick.up.isPressed){
+        if(Keyboard.current.wKey.isPressed){
             playerAnimator.SetBool("isWalkingUp", true);
-        } else  if(Keyboard.current.sKey.isPressed || Gamepad.current.leftStick.down.isPressed){
+        } else  if(Keyboard.current.sKey.isPressed){
             playerAnimator.SetBool("isWalkingDown", true);
-        } else  if(Keyboard.current.aKey.isPressed || Gamepad.current.leftStick.left.isPressed){
+        } else  if(Keyboard.current.aKey.isPressed){
             playerAnimator.SetBool("isWalkingSide", true);
               transform.rotation = Quaternion.Euler(0, 180, 0);
-        } else if (Keyboard.current.dKey.isPressed || Gamepad.current.leftStick.right.isPressed){
+        } else if (Keyboard.current.dKey.isPressed){
             playerAnimator.SetBool("isWalkingSide", true);
              transform.rotation = Quaternion.Euler(0, 0, 0);
         }
@@ -77,57 +73,31 @@ public class PlayerMovement : MonoBehaviour
 
     void TurnThePlayer() 
     {
-        if(Gamepad.current == null){
-            Vector2 mousePos = Mouse.current.position.ReadValue();
-            Vector2 objectPos = Camera.main.WorldToScreenPoint(transform.position);
-            mousePos.x = mousePos.x - objectPos.x;
-            mousePos.y = mousePos.y - objectPos.y;
-            var angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
-            if (playerAnimator.GetCurrentAnimatorStateInfo(0).IsName("Idle") || GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("UseSkill"))
+        Vector2 mousePos = Mouse.current.position.ReadValue();
+        Vector2 objectPos = Camera.main.WorldToScreenPoint(transform.position);
+        mousePos.x = mousePos.x - objectPos.x;
+        mousePos.y = mousePos.y - objectPos.y;
+        var angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+        if (playerAnimator.GetCurrentAnimatorStateInfo(0).IsName("Idle") || GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("UseSkill"))
+        {
+            //Adjust player sprite depending on angle
+            if (angle < -225 && angle > -315 || angle < 135 && angle > 45)
             {
-                //Adjust player sprite depending on angle
-                if (angle < -225 && angle > -315 || angle < 135 && angle > 45)
-                {
-                    spriteRenderer.sprite = Up;
-                }
-                else if (angle >= -45 && angle <= 45)
-                {
-                    spriteRenderer.sprite = Side;
-                    transform.rotation = Quaternion.Euler(Vector3.zero);
-                }
-                else if (angle < -45 && angle > -125)
-                {
-                    spriteRenderer.sprite = Down;
-                }
-                else
-                {
-                    spriteRenderer.sprite = Side;
-                    transform.rotation = Quaternion.Euler(0, 180, 0);
-                }
+                spriteRenderer.sprite = Up;
             }
-        } else {
-            Vector2 rightStickDir = rotateInput;
-            if (playerAnimator.GetCurrentAnimatorStateInfo(0).IsName("Idle") || GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("UseSkill"))
+            else if (angle >= -45 && angle <= 45)
             {
-                //Adjust player sprite depending on angle
-                if (rotateInput.y > 0)
-                {
-                    spriteRenderer.sprite = Up;
-                }
-                else if (rotateInput.x > 0)
-                {
-                    spriteRenderer.sprite = Side;
-                    transform.rotation = Quaternion.Euler(Vector3.zero);
-                }
-                else if (rotateInput.y < 0)
-                {
-                    spriteRenderer.sprite = Down;
-                }
-                else
-                {
-                    spriteRenderer.sprite = Side;
-                    transform.rotation = Quaternion.Euler(0, 180, 0);
-                }
+                spriteRenderer.sprite = Side;
+                transform.rotation = Quaternion.Euler(Vector3.zero);
+            }
+            else if (angle < -45 && angle > -125)
+            {
+                spriteRenderer.sprite = Down;
+            }
+            else
+            {
+                spriteRenderer.sprite = Side;
+                transform.rotation = Quaternion.Euler(0, 180, 0);
             }
         }
     }
