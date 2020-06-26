08942860517c7ae7fa32a7f44b5fa7b902e12a97
Luis Moßburger <lmossburger@t-online.de>
2020-06-26 11:46:25 +0200

    Ability Cooldown
    Abilities now have a visual, radial cooldown through a dark overlay instead of numbers.

--- Run_Kitty_Run/Assets/Scripts/AbilityCoolDown.cs
+++ Run_Kitty_Run/Assets/Scripts/AbilityCoolDown.cs
@@ -1,13 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using System.Reflection;
 
 public class AbilityCoolDown : MonoBehaviour
 {
 
     public string abilityButtonAxisName = "Fire1";
-    //public Image darkMask;
     public Text coolDownTextDisplay;
+    public Image cooldownOverlay;
 
     [SerializeField] private Ability ability;
     private GameObject player;
@@ -49,6 +50,7 @@ public class AbilityCoolDown : MonoBehaviour
                 {
                     Debug.Log("Ability triggered: " + ability.name);
                     ButtonTriggered();
+                    cooldownOverlay.fillAmount = 1;
                 }
             }
             else
@@ -65,15 +67,15 @@ public class AbilityCoolDown : MonoBehaviour
     private void AbilityReady()
     {
         coolDownTextDisplay.enabled = false;
-        //darkMask.enabled = false;
+        cooldownOverlay.fillAmount = 0;
     }
 
     private void CoolDown()
     {
         coolDownTimeLeft -= Time.deltaTime;
-        float roundedCd = Mathf.Round(coolDownTimeLeft);
-        coolDownTextDisplay.text = coolDownTimeLeft.ToString("F1");
-        //darkMask.fillAmount = (coolDownTimeLeft / coolDownDuration);
+        //float roundedCd = Mathf.Round(coolDownTimeLeft);
+        //coolDownTextDisplay.text = coolDownTimeLeft.ToString("F1");
+        cooldownOverlay.fillAmount = coolDownTimeLeft/coolDownDuration;
 
     }
 
