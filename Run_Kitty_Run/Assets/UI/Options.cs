695b10baa08ca30354bb2151a358f8a1b37065a5
Luis Moßburger <lmossburger@t-online.de>
2020-07-14 14:12:10 +0200

    Added options menu
    Resolving #17 by adding an options menu for volume, graphics quality and fullscreen. To Do: Check Quality & Sound - really working? Difference is marginally...

---
+++ Run_Kitty_Run/Assets/UI/Options.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class Options : MonoBehaviour
+{
+    public GameObject optionsMenu;
+    public Button fullscreenButton;
+    public Button lowButton;
+    public Button midButton;
+    public Button highButton;
+    public Sprite buttonActive;
+    public Sprite buttonInactive;
+    public AudioMixer audioMixer;
+
+    //Open Menu
+    public void OpenMenu()
+    {
+        optionsMenu.SetActive(true);
+    }
+
+    //Close Menu
+    public void CloseMenu()
+    {
+        optionsMenu.SetActive(false);
+    }
+
+    //Set volume 
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("volume", volume);
+    }
+
+    //Set graphics quality
+    public void SetQualityLow()
+    {
+        QualitySettings.SetQualityLevel(0);
+        lowButton.GetComponent<Image>().sprite = buttonActive;
+        midButton.GetComponent<Image>().sprite = buttonInactive;
+        highButton.GetComponent<Image>().sprite = buttonInactive;
+    }
+
+    public void SetQualityMid()
+    {
+        QualitySettings.SetQualityLevel(1);
+        midButton.GetComponent<Image>().sprite = buttonActive;
+        lowButton.GetComponent<Image>().sprite = buttonInactive;
+        highButton.GetComponent<Image>().sprite = buttonInactive;
+    }
+
+    public void SetQualityHigh()
+    {
+        QualitySettings.SetQualityLevel(2);
+        highButton.GetComponent<Image>().sprite = buttonActive;
+        midButton.GetComponent<Image>().sprite = buttonInactive;
+        lowButton.GetComponent<Image>().sprite = buttonInactive;
+    }
+
+    //Set fullscreen
+    public void ToggleFullscreen()
+    {
+        if (Screen.fullScreen)
+        {
+            Screen.fullScreen = false;
+            fullscreenButton.GetComponent<Image>().sprite = buttonInactive;
+        } else
+        {
+            Screen.fullScreen = true;
+            fullscreenButton.GetComponent<Image>().sprite = buttonActive;
+        }
+    }
+}
