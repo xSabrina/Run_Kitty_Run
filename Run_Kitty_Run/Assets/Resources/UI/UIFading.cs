ac7f7c469992b430b42a1b0fc576af8609d6ff0b
Luis Moßburger <lmossburger@t-online.de>
2020-08-07 22:03:32 +0200

    Level atmosphere & sound/UI updates
    Level atmosphere #93 :
    * Sunbeams & clouds on day levels
    * fireflies in dawn levels
    * no effects in night levels as those would be pretty overdone...
    
    Sound updates
    * music does not replay in level 6
    * changed sounds for shooting enemies
    * background music is now not as loud
    
    UI
    * menu scaling fix

---
+++ Run_Kitty_Run/Assets/Resources/UI/UIFading.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIFading : MonoBehaviour
+{
+
+    public CanvasGroup canvasGroup;
+
+    public void FadeIn(Component comp)
+    {
+        StartCoroutine(FadeCanvas(canvasGroup, canvasGroup.alpha, 1));
+    }
+
+    public void FadeOut(Component comp)
+    {
+        StartCoroutine(FadeCanvas(canvasGroup, canvasGroup.alpha, 0));
+    }
+
+    IEnumerator FadeCanvas(CanvasGroup cg, float start, float end, float lerpTime = 0.5f)
+    {
+        float _timeStartedLerping = Time.time;
+        float timeSinceStarted = Time.time - _timeStartedLerping;
+        float percentageComplete = timeSinceStarted / lerpTime;
+
+        while (true)
+        {
+            timeSinceStarted = Time.time - _timeStartedLerping;
+            percentageComplete = timeSinceStarted / lerpTime;
+
+            float currentValue = Mathf.Lerp(start, end, percentageComplete);
+
+            cg.alpha = currentValue;
+
+            if (percentageComplete >= 1)
+            {
+                if (end == 0)
+                {
+                    gameObject.SetActive(false);
+                } else if (end == 1)
+                {
+                    gameObject.SetActive(true);
+                }
+                break;
+            }
+
+            yield return new WaitForEndOfFrame();
+        }
+        print("done fading");
+
+    }
+}
