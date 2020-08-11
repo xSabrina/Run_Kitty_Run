89b4b235a5bd6f193acd719ace1a741447bf5001
Luis Moßburger <lmossburger@t-online.de>
2020-08-11 09:30:59 +0200

    Animated main menu
    FadeIn for Main Menu fitting with music entry

--- Run_Kitty_Run/Assets/Resources/UI/UIFading.cs
+++ Run_Kitty_Run/Assets/Resources/UI/UIFading.cs
@@ -5,48 +5,10 @@ using UnityEngine;
 public class UIFading : MonoBehaviour
 {
 
-    public CanvasGroup canvasGroup;
+    public float delay = 0f;
 
-    public void FadeIn(Component comp)
+    void Start ()
     {
-        StartCoroutine(FadeCanvas(canvasGroup, canvasGroup.alpha, 1));
-    }
-
-    public void FadeOut(Component comp)
-    {
-        StartCoroutine(FadeCanvas(canvasGroup, canvasGroup.alpha, 0));
-    }
-
-    IEnumerator FadeCanvas(CanvasGroup cg, float start, float end, float lerpTime = 0.5f)
-    {
-        float _timeStartedLerping = Time.time;
-        float timeSinceStarted = Time.time - _timeStartedLerping;
-        float percentageComplete = timeSinceStarted / lerpTime;
-
-        while (true)
-        {
-            timeSinceStarted = Time.time - _timeStartedLerping;
-            percentageComplete = timeSinceStarted / lerpTime;
-
-            float currentValue = Mathf.Lerp(start, end, percentageComplete);
-
-            cg.alpha = currentValue;
-
-            if (percentageComplete >= 1)
-            {
-                if (end == 0)
-                {
-                    gameObject.SetActive(false);
-                } else if (end == 1)
-                {
-                    gameObject.SetActive(true);
-                }
-                break;
-            }
-
-            yield return new WaitForEndOfFrame();
-        }
-        print("done fading");
-
+        LeanTween.alphaCanvas(gameObject.GetComponent<CanvasGroup>(), 1f, 1f).setDelay(delay);
     }
 }
