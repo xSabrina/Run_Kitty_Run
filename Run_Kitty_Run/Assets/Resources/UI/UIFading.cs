5644abc6788df40e96b786ee5cf62d7b8cd642ee
Luis Moßburger <lmossburger@t-online.de>
2020-08-11 09:55:14 +0200

    Animation for Highscores
    * Added fadeIn for Win Screen
    * complete time fades in slightly later for tension reasons ;)
    * animated highscore table

--- Run_Kitty_Run/Assets/Resources/UI/UIFading.cs
+++ Run_Kitty_Run/Assets/Resources/UI/UIFading.cs
@@ -6,9 +6,10 @@ public class UIFading : MonoBehaviour
 {
 
     public float delay = 0f;
+    public float duration = 0.6f;
 
     void Start ()
     {
-        LeanTween.alphaCanvas(gameObject.GetComponent<CanvasGroup>(), 1f, 0.6f).setEase(LeanTweenType.easeInQuad).setDelay(delay);
+        LeanTween.alphaCanvas(gameObject.GetComponent<CanvasGroup>(), 1f, duration).setEase(LeanTweenType.easeInQuad).setDelay(delay);
     }
 }
