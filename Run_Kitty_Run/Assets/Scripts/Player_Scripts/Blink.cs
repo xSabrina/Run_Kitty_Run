8c1c15bfe7b67144e3ec16fdf68708ddc62822bf
flexwend <felix.wende@web.de>
2020-08-12 18:09:08 +0200

    Update Blink.cs
    - fixed blink out of the map
    - code cleanup

--- Run_Kitty_Run/Assets/Scripts/Player_Scripts/Blink.cs
+++ Run_Kitty_Run/Assets/Scripts/Player_Scripts/Blink.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System;
 
 [CreateAssetMenu(menuName = "Abilities/Blink")]
 public class Blink : Ability
@@ -15,6 +16,11 @@ public class Blink : Ability
     private Animator animator;
     private AudioSource audioSource;
 
+    private float colliderOffsetX;
+    private float colliderOffsetY;
+    private float colliderSizeHalfX;
+    private float colliderSizeHalfY;
+
     private float blinkDuration = 0.35F;
 
     public override void Initialize(GameObject obj)
@@ -24,6 +30,11 @@ public class Blink : Ability
         playerCollider = launcher.player.GetComponent<CapsuleCollider2D>();
         animator = launcher.player.GetComponent<Animator>();
         audioSource = launcher.player.GetComponent<AudioSource>();
+
+        colliderOffsetX = playerCollider.offset.x * playerTransform.localScale.x;
+        colliderOffsetY = playerCollider.offset.y * playerTransform.localScale.y;
+        colliderSizeHalfX = playerCollider.size.x * playerTransform.localScale.x / 2;
+        colliderSizeHalfY = playerCollider.size.y * playerTransform.localScale.y / 2;
     }
 
     public override void TriggerAbility()
@@ -34,41 +45,37 @@ public class Blink : Ability
         PlayerAbilities.instance.StartCoroutine(BlinkingTime(blinkDuration/2));
     }
 
-    void CalculateBlink(){
+    void CalculateBlink()
+    {
         bool triggered = false;
-        Transform playerTransform = launcher.player.GetComponent<Transform>();
-        CapsuleCollider2D playerCollider = launcher.player.GetComponent<CapsuleCollider2D>();
-        int hitint = Physics2D.Raycast(playerTransform.position, launcher.spawnPoint.transform.up, filter.NoFilter(), res, aRange);
-        float adjustedBlinkRange;
-        foreach(RaycastHit2D hit in res)
+
+        // check for borders in blink direction
+        int hitint = Physics2D.Raycast(launcher.spawnPoint.position, launcher.spawnPoint.transform.up, filter.NoFilter(), res, aRange); 
+        foreach (RaycastHit2D hit in res)
         {
             if(hit.collider.tag == "Border")
             {
-                Debug.Log(hit.collider.name);
-                // blink direction is downwards
-                if (launcher.spawnPoint.transform.up.y <= 0)
-                {
-                    // adjust blinkrange according to size of player collider
-                    // times 10 because player is scaled by 10
-                    // devided by 2 because origin of collider is in the middle of the collider
-                    adjustedBlinkRange = hit.distance - (playerCollider.size.y * 10) / 2;
-
-                    playerTransform.position += launcher.spawnPoint.transform.up.normalized * adjustedBlinkRange;
-                }
-                else
-                {
-                    adjustedBlinkRange = hit.distance - (playerCollider.size.x * 10) / 2;
-                    playerTransform.position += launcher.spawnPoint.transform.up.normalized * adjustedBlinkRange;
-                }
+                playerTransform.position = PositionAfterBlink(hit.point);
                 triggered = true;
                 break;
             }
         }
+
         // no border in blink direction
         if (!triggered)
         {
-            playerTransform.position += launcher.spawnPoint.transform.up.normalized * aRange;
+            Vector2 hitPoint = launcher.spawnPoint.position + launcher.spawnPoint.transform.up.normalized * aRange;
+            playerTransform.position = PositionAfterBlink(hitPoint);
         }
+    }
+
+    private Vector2 PositionAfterBlink(Vector2 hitPoint)
+    {
+        // adjust position after blink with player collider offset and size
+        Vector2 hitPointToPlayer = (Vector2)playerTransform.position - hitPoint;
+        int xSign = Math.Sign(hitPointToPlayer.x);
+        int ySign = Math.Sign(hitPointToPlayer.y);
+        return hitPoint + new Vector2(-colliderOffsetX + xSign * colliderSizeHalfX, -colliderOffsetX + ySign * colliderSizeHalfY);
     }
 
     void PlayAnimation ()
@@ -82,7 +89,8 @@ public class Blink : Ability
     IEnumerator WaitingTime(float Float)
     {
         yield return new WaitForSeconds(Float);
-        if(!animator.GetCurrentAnimatorStateInfo(0).IsName("Die")){
+        if (!animator.GetCurrentAnimatorStateInfo(0).IsName("Die"))
+        {
             launcher.player.GetComponent<PlayerMovement>().enabled = true;
             launcher.player.GetComponent<Collider2D>().enabled = true;
         }
